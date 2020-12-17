e91baf2ef98e7615ab0ee4c8b198104841da08ae
´jonason <jonason22@hotmail.se>
2020-12-17 22:22:46 +0100

    Solved day3 part 1

--- 3/main.cpp
+++ 3/main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <cstring>
+
 #include <vector>
 #include <sstream>
 
 using namespace std;
 
-char getMazeCharacter(int i, int j) {
+string getMazeCharacter(int rowIndex, int virtualColIndex, vector<string>& map, int nrCols) {
+    string c;
+    int colIndex = virtualColIndex % nrCols;
+    cout << colIndex;
+    c = map.at(rowIndex).at(colIndex);
     
+    return c;
 }
 
 int main(){
@@ -21,9 +27,27 @@ int main(){
         exit(1);
     }
 
+    int i = 0;
     while(!inFile.eof()) {
-        getline(inFile, map.at(0));      
+        string s;
+        inFile >> s;
+        map.push_back(s);
+        i++;
+
     }
     inFile.close();
 
+    int nrCols = map.at(0).size();
+    int nrRows = map.size();
+    int treeCounter = 0;
+
+
+    for (int j = 0; j < nrRows-1; j++) {
+        string c = getMazeCharacter(j, 3*j, map, nrCols);
+        
+        if(!strcmp(c.c_str(), "#")) {
+            treeCounter++;
+        }
+    }
+    cout << "The number of encountered trees are : " << treeCounter << endl;
 }
