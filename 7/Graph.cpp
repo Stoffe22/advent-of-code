64fa7a73e6bce4f82adf9b622e6359598f842346
Christofer Jonason <c.jonason22@gmail.com>
2021-01-18 21:24:51 +0100

    Changed from struct to class and started implementation

--- 7/Graph.cpp
+++ 7/Graph.cpp
@@ -1,18 +1,55 @@
 #include <string>
 #include "Graph.hpp"
+#include <boost/algorithm/string.hpp>
 
-//#include <range/v3/view/split.hpp>
 
-void node::addNeighbour(const std::string& name)
+void Node::addNeighbour(const std::string& name)
 {
     neighbours.push_back(std::move(name));
 }
 
-graph::graph(const std::string& filename) {
+Node::Node(std::string&& name)
+    : name(name)
+{
+}
+
+const std::string& Node::getName() { return name;}
+
+const bool Graph::nodeExist(const std::string& name)
+{
+    for (auto& node: nodes)
+        if (node.getName() == name)
+        {
+            return true;
+        }
+    return false;
+}
+
+Graph::Graph(const std::string& filename) {
     std::string line; 
     std::ifstream inFile(filename.c_str());
-    while(!inFile.eof()) {
+    while(!inFile.eof()) 
+    {
         getline(inFile, line);
+        
+        std::vector<std::string> results;
+        boost::split(results, line, [] (char c){return c == ', ' || isdigit(c);});
+        for (std::vector<std::string>::iterator nameIter = results.begin();
+             nameIter != results.end(); nameIter++)
+            if (nameIter == results.begin())
+            {   
+                Node n(std::move(*nameIter));
+                insertNode(n);
+            }
+            else
+            {
+                // getNode by intex
+                // then add neighbour
 
+            }
+            
+
+
+            
     }
 }
